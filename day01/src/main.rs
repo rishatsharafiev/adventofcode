1b75b4e354072aa65a60495eab3f1453e7a918ad
Rishat Sharafiev <rishatsharafiev@ya.ru>
2022-12-07 22:45:32 +0200

    add day01

---
+++ day01/src/main.rs
@@ -0,0 +1,31 @@
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
+
+fn main() -> io::Result<()> {
+    let file = File::open("input")?;
+    let reader = BufReader::new(file);
+    let mut sum: i32 = 0;
+    let mut counter: i32 = 0;
+
+    for line in reader.lines() {
+        match line {
+            Ok(s) => {
+                if s.is_empty() {
+                    if counter > sum {
+                        sum = counter;
+                        counter = 0;
+                    }
+                } else {
+                    counter += s.trim().parse::<i32>().unwrap();
+                }
+            }
+            Err(e) => {
+                println!("{:?}", e);
+            }
+        }
+    }
+
+    println!("{}", sum);
+
+    Ok(())
+}
